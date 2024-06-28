04bc29c3092ae0579de42e94bf6cf9511b95b7b3
Iris <wiktoriajarema301@gmail.com>
2024-06-28 20:18:46 +0200

    Dodano funkcje zarządzania portfelem z użyciem Truflation API

--- src/views/admin/marketplace/index.jsx
+++ src/views/admin/marketplace/index.jsx
@@ -1,28 +1,4 @@
-/*!
-  _   _  ___  ____  ___ ________  _   _   _   _ ___   
- | | | |/ _ \|  _ \|_ _|__  / _ \| \ | | | | | |_ _| 
- | |_| | | | | |_) || |  / / | | |  \| | | | | || | 
- |  _  | |_| |  _ < | | / /| |_| | |\  | | |_| || |
- |_| |_|\___/|_| \_\___/____\___/|_| \_|  \___/|___|
-                                                                                                                                                                                                                                                                                                                                       
-=========================================================
-* Horizon UI - v1.1.0
-=========================================================
-
-* Product Page: https://www.horizon-ui.com/
-* Copyright 2023 Horizon UI (https://www.horizon-ui.com/)
-
-* Designed and Coded by Simmmple
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/
-
 import React from "react";
-
-// Chakra imports
 import {
   Box,
   Button,
@@ -34,279 +10,245 @@ import {
   SimpleGrid,
 } from "@chakra-ui/react";
 
-// Custom components
+// Importowanie istniejących komponentów i danych
 import Banner from "views/admin/marketplace/components/Banner";
 import HistoryItem from "views/admin/marketplace/components/HistoryItem";
-import NFT from "components/card/NFT";
-import Card from "components/card/Card.js";
 import Property from "views/admin/marketplace/components/Property";
+import Card from "components/card/Card.js";
 
-// Assets
+// Importowanie obrazów nieruchomości
 import Property1 from "assets/img/properties/Property1.png";
 import Property2 from "assets/img/properties/Property2.png";
 import Property3 from "assets/img/properties/Property3.png";
 import Property4 from "assets/img/properties/Property4.png";
 import Property5 from "assets/img/properties/Property5.png";
 import Property6 from "assets/img/properties/Property6.png";
+
+// Importowanie awatarów
 import Avatar1 from "assets/img/avatars/avatar1.png";
 import Avatar2 from "assets/img/avatars/avatar2.png";
 import Avatar3 from "assets/img/avatars/avatar3.png";
 import Avatar4 from "assets/img/avatars/avatar4.png";
-import tableDataTopCreators from "views/admin/marketplace/variables/tableDataTopCreators.json";
-import { tableColumnsTopCreators } from "views/admin/marketplace/variables/tableColumnsTopCreators";
 
-
+// Konwersje do kryptowalut
 const conversionRates = {
-  BTC: 0.000025, // Example conversion rate, 1 USD = 0.000025 BTC
-  ETH: 0.0004,   // Example conversion rate, 1 USD = 0.0004 ETH
-  LTC: 0.005,    // Example conversion rate, 1 USD = 0.005 LTC
+  BTC: 0.000025, // Przykładowe kursy wymiany, 1 USD = 0.000025 BTC
+  ETH: 0.0004,   // Przykładowe kursy wymiany, 1 USD = 0.0004 ETH
+  XRP: 0.005,    // Przykładowe kursy wymiany, 1 USD = 0.005 XRP
+  LTC: 0.005,    // Przykładowe kursy wymiany, 1 USD = 0.005 LTC
+  BCH: 0.01,     // Przykładowe kursy wymiany, 1 USD = 0.01 BCH
+  BNB: 0.1       // Przykładowe kursy wymiany, 1 USD = 0.1 BNB
 };
 
+// Funkcja do konwersji ceny do kryptowaluty
 const convertToCrypto = (usdPrice, crypto) => {
   return (usdPrice * conversionRates[crypto]).toFixed(6);
 };
 
-
-
 export default function Marketplace() {
-  // Chakra Color Mode
+  // Ustawienia kolorów Chakra
   const textColor = useColorModeValue("secondaryGray.900", "white");
   const textColorBrand = useColorModeValue("brand.500", "white");
+
   return (
     <Box pt={{ base: "180px", md: "80px", xl: "80px" }}>
-      {/* Main Fields */}
+      {/* Główne pola */}
       <Grid
-        mb='20px'
+        mb="20px"
         gridTemplateColumns={{ xl: "repeat(3, 1fr)", "2xl": "1fr 0.46fr" }}
         gap={{ base: "20px", xl: "20px" }}
-        display={{ base: "block", xl: "grid" }}>
+        display={{ base: "block", xl: "grid" }}
+      >
         <Flex
-          flexDirection='column'
-          gridArea={{ xl: "1 / 1 / 2 / 3", "2xl": "1 / 1 / 2 / 2" }}>
+          flexDirection="column"
+          gridArea={{ xl: "1 / 1 / 2 / 3", "2xl": "1 / 1 / 2 / 2" }}
+        >
           <Banner />
-          <Flex direction='column'>
+          <Flex direction="column">
             <Flex
-              mt='45px'
-              mb='20px'
-              justifyContent='space-between'
+              mt="45px"
+              mb="20px"
+              justifyContent="space-between"
               direction={{ base: "column", md: "row" }}
-              align={{ base: "start", md: "center" }}>
-              <Text color={textColor} fontSize='2xl' ms='24px' fontWeight='700'>
+              align={{ base: "start", md: "center" }}
+            >
+              <Text color={textColor} fontSize="2xl" ms="24px" fontWeight="700">
                 Available Properties
               </Text>
               <Flex
-                align='center'
-                me='20px'
+                align="center"
+                me="20px"
                 ms={{ base: "24px", md: "0px" }}
-                mt={{ base: "20px", md: "0px" }}>
+                mt={{ base: "20px", md: "0px" }}
+              >
                 <Link
                   color={textColorBrand}
-                  fontWeight='500'
+                  fontWeight="500"
                   me={{ base: "34px", md: "44px" }}
-                  to='#apartments'>
+                  to="#apartments"
+                >
                   Apartments
                 </Link>
                 <Link
                   color={textColorBrand}
-                  fontWeight='500'
+                  fontWeight="500"
                   me={{ base: "34px", md: "44px" }}
-                  to='#houses'>
+                  to="#houses"
+                >
                   Houses
                 </Link>
                 <Link
                   color={textColorBrand}
-                  fontWeight='500'
+                  fontWeight="500"
                   me={{ base: "34px", md: "44px" }}
-                  to='#commercial'>
+                  to="#commercial"
+                >
                   Commercial
                 </Link>
-                <Link color={textColorBrand} fontWeight='500' to='#land'>
+                <Link color={textColorBrand} fontWeight="500" to="#land">
                   Land
                 </Link>
               </Flex>
             </Flex>
-            <SimpleGrid columns={{ base: 1, md: 3 }} gap='20px'>
+            <SimpleGrid columns={{ base: 1, md: 3 }} gap="20px">
+              {/* Komponenty Property z informacjami o tokenach */}
               <Property
-                name='Luxury Apartment'
-                location='New York, NY'
-                agents={[
-                  Avatar1,
-                  Avatar2,
-                  Avatar3,
-                  Avatar4,
-                  Avatar1,
-                  Avatar1,
-                  Avatar1,
-                  Avatar1,
-                ]}
+                name="Luxury Apartment"
+                location="New York, NY"
+                agents={[Avatar1, Avatar2, Avatar3, Avatar4]}
                 image={Property1}
                 price="2.5 BTC"
-                details='#'
+                tokenPrice={convertToCrypto(2.5, "BTC")}
+                details="#"
               />
               <Property
-                name='Suburban House'
-                location='Los Angeles, CA'
-                agents={[
-                  Avatar1,
-                  Avatar2,
-                  Avatar3,
-                  Avatar4,
-                  Avatar1,
-                  Avatar1,
-                  Avatar1,
-                  Avatar1,
-                ]}
+                name="Suburban House"
+                location="Los Angeles, CA"
+                agents={[Avatar1, Avatar2, Avatar3, Avatar4]}
                 image={Property2}
                 price="15 ETH"
-                details='#'
+                tokenPrice={convertToCrypto(15, "ETH")}
+                details="#"
               />
               <Property
-                name='Downtown Office Space'
-                location='Chicago, IL'
-                agents={[
-                  Avatar1,
-                  Avatar2,
-                  Avatar3,
-                  Avatar4,
-                  Avatar1,
-                  Avatar1,
-                  Avatar1,
-                  Avatar1,
-                ]}
+                name="Downtown Office Space"
+                location="Chicago, IL"
+                agents={[Avatar1, Avatar2, Avatar3, Avatar4]}
                 image={Property3}
                 price="1,500 XRP"
-                details='#'
+                tokenPrice={convertToCrypto(1500, "XRP")}
+                details="#"
               />
             </SimpleGrid>
             <Text
-              mt='45px'
-              mb='36px'
+              mt="45px"
+              mb="36px"
               color={textColor}
-              fontSize='2xl'
-              ms='24px'
-              fontWeight='700'>
+              fontSize="2xl"
+              ms="24px"
+              fontWeight="700"
+            >
               Recently Added
             </Text>
             <SimpleGrid
               columns={{ base: 1, md: 3 }}
-              gap='20px'
-              mb={{ base: "20px", xl: "0px" }}>
+              gap="20px"
+              mb={{ base: "20px", xl: "0px" }}
+            >
               <Property
-                name='Cozy Cottage'
-                location='Austin, TX'
-                agents={[
-                  Avatar1,
-                  Avatar2,
-                  Avatar3,
-                  Avatar4,
-                  Avatar1,
-                  Avatar1,
-                  Avatar1,
-                  Avatar1,
-                ]}
+                name="Cozy Cottage"
+                location="Austin, TX"
+                agents={[Avatar1, Avatar2, Avatar3, Avatar4]}
                 image={Property4}
                 price="5 LTC"
-                details='#'
+                tokenPrice={convertToCrypto(5, "LTC")}
+                details="#"
               />
               <Property
-                name='Modern Villa'
-                location='Miami, FL'
-                agents={[
-                  Avatar1,
-                  Avatar2,
-                  Avatar3,
-                  Avatar4,
-                  Avatar1,
-                  Avatar1,
-                  Avatar1,
-                  Avatar1,
-                ]}
+                name="Modern Villa"
+                location="Miami, FL"
+                agents={[Avatar1, Avatar2, Avatar3, Avatar4]}
                 image={Property5}
                 price="10 BCH"
-                details='#'
+                tokenPrice={convertToCrypto(10, "BCH")}
+                details="#"
               />
               <Property
-                name='Country Estate'
-                location='Nashville, TN'
-                agents={[
-                  Avatar1,
-                  Avatar2,
-                  Avatar3,
-                  Avatar4,
-                  Avatar1,
-                  Avatar1,
-                  Avatar1,
-                  Avatar1,
-                ]}
+                name="Country Estate"
+                location="Nashville, TN"
+                agents={[Avatar1, Avatar2, Avatar3, Avatar4]}
                 image={Property6}
                 price="100 BNB"
-                details='#'
+                tokenPrice={convertToCrypto(100, "BNB")}
+                details="#"
               />
             </SimpleGrid>
           </Flex>
         </Flex>
         <Flex
-          flexDirection='column'
-          gridArea={{ xl: "1 / 3 / 2 / 4", "2xl": "1 / 2 / 2 / 3" }}>
-          <Card p='0px'>
+          flexDirection="column"
+          gridArea={{ xl: "1 / 3 / 2 / 4", "2xl": "1 / 2 / 2 / 3" }}
+        >
+          {/* Tutaj znajduje się komponent History */}
+          <Card p="0px">
             <Flex
               align={{ sm: "flex-start", lg: "center" }}
-              justify='space-between'
-              w='100%'
-              px='22px'
-              py='18px'>
-              <Text color={textColor} fontSize='xl' fontWeight='600'>
+              justify="space-between"
+              w="100%"
+              px="22px"
+              py="18px"
+            >
+              <Text color={textColor} fontSize="xl" fontWeight="600">
                 History
               </Text>
-              <Button variant='action'>See all</Button>
+              <Button variant="action">See all</Button>
             </Flex>
-
+            {/* HistoryItems tutaj */}
             <HistoryItem
-              name='Modern Villa'
-              location='Miami, FL'
-              date='30s ago'
+              name="Modern Villa"
+              location="Miami, FL"
+              date="30s ago"
               image={Property5}
               price="15 ETH"
             />
             <HistoryItem
-              name='Luxury Apartment'
-              location='New York, NY'
-              date='58s ago'
+              name="Luxury Apartment"
+              location="New York, NY"
+              date="58s ago"
               image={Property1}
               price="2.5 BTC"
             />
             <HistoryItem
-              name='Suburban House'
-              location='Los Angeles, CA'
-              date='1m ago'
+              name="Suburban House"
+              location="Los Angeles, CA"
+              date="1m ago"
               image={Property2}
               price="15 ETH"
             />
             <HistoryItem
-              name='Cozy Cottage'
-              location='Austin, TX'
-              date='1m ago'
+              name="Cozy Cottage"
+              location="Austin, TX"
+              date="1m ago"
               image={Property4}
               price="5 LTC"
             />
             <HistoryItem
-              name='Downtown Office Space'
-              location='Chicago, IL'
-              date='2m ago'
+              name="Downtown Office Space"
+              location="Chicago, IL"
+              date="2m ago"
               image={Property3}
               price="1,500 XRP"
             />
             <HistoryItem
-              name='Country Estate'
-              location='Nashville, TN'
-              date='3m ago'
+              name="Country Estate"
+              location="Nashville, TN"
+              date="3m ago"
               image={Property6}
               price="100 BNB"
             />
           </Card>
         </Flex>
       </Grid>
-      {/* Delete Product */}
     </Box>
   );
 }
--- src/views/admin/profile/index.jsx
+++ src/views/admin/profile/index.jsx
@@ -1,118 +1,181 @@
-/*!
-  _   _  ___  ____  ___ ________  _   _   _   _ ___   
- | | | |/ _ \|  _ \|_ _|__  / _ \| \ | | | | | |_ _| 
- | |_| | | | | |_) || |  / / | | |  \| | | | | || | 
- |  _  | |_| |  _ < | | / /| |_| | |\  | | |_| || |
- |_| |_|\___/|_| \_\___/____\___/|_| \_|  \___/|___|
-                                                                                                                                                                                                                                                                                                                                       
-=========================================================
-* Horizon UI - v1.1.0
-=========================================================
+import React, { useState } from "react";
+import {
+  Box,
+  Button,
+  Flex,
+  Grid,
+  Text,
+  Input,
+  InputGroup,
+  InputLeftElement,
+  Select,
+  SimpleGrid,
+  Stack,
+} from "@chakra-ui/react";
+import { SearchIcon } from "@chakra-ui/icons";
+import Card from "components/card/Card.js";
 
-* Product Page: https://www.horizon-ui.com/
-* Copyright 2023 Horizon UI (https://www.horizon-ui.com/)
+// Sample transaction data
+const sampleTransactions = [
+  {
+    id: 1,
+    name: "Luxury Apartment",
+    location: "New York, NY",
+    date: "2023-01-15",
+    price: 1200000,
+    type: "purchase",
+  },
+  {
+    id: 2,
+    name: "Suburban House",
+    location: "Los Angeles, CA",
+    date: "2023-02-20",
+    price: 850000,
+    type: "sale",
+  },
+  {
+    id: 3,
+    name: "Downtown Office Space",
+    location: "Chicago, IL",
+    date: "2023-03-10",
+    price: 3500000,
+    type: "purchase",
+  },
+  {
+    id: 4,
+    name: "Cozy Cottage",
+    location: "Austin, TX",
+    date: "2023-04-05",
+    price: 450000,
+    type: "sale",
+  },
+  {
+    id: 5,
+    name: "Modern Villa",
+    location: "Miami, FL",
+    date: "2023-05-25",
+    price: 2300000,
+    type: "purchase",
+  },
+  {
+    id: 6,
+    name: "Country Estate",
+    location: "Nashville, TN",
+    date: "2023-06-30",
+    price: 1750000,
+    type: "sale",
+  },
+];
 
-* Designed and Coded by Simmmple
+// Function to convert prices to cryptocurrencies
+const convertToCrypto = (price) => {
+  const btcRate = 32000; // Example BTC rate
+  const ethRate = 2100; // Example ETH rate
+  const usdToBtc = price / btcRate;
+  const usdToEth = price / ethRate;
+  return {
+    btc: usdToBtc.toFixed(4),
+    eth: usdToEth.toFixed(4),
+  };
+};
 
-=========================================================
+const Transactions = () => {
+  const [transactions, setTransactions] = useState(sampleTransactions);
+  const [searchQuery, setSearchQuery] = useState("");
+  const [filterType, setFilterType] = useState("all");
 
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+  // Function to handle search input change
+  const handleSearchChange = (e) => {
+    setSearchQuery(e.target.value);
+  };
 
-*/
+  // Function to handle filter type change
+  const handleFilterTypeChange = (e) => {
+    setFilterType(e.target.value);
+  };
 
-// Chakra imports
-import { Box, Grid } from "@chakra-ui/react";
+  // Filtered transactions based on search query and filter type
+  const filteredTransactions = transactions.filter((transaction) => {
+    const matchesSearchQuery =
+      transaction.name.toLowerCase().includes(searchQuery.toLowerCase()) ||
+      transaction.location.toLowerCase().includes(searchQuery.toLowerCase());
+    const matchesFilterType =
+      filterType === "all" || transaction.type === filterType;
+    return matchesSearchQuery && matchesFilterType;
+  });
 
-// Custom components
-import Banner from "views/admin/profile/components/Banner";
-import General from "views/admin/profile/components/General";
-import Notifications from "views/admin/profile/components/Notifications";
-import Projects from "views/admin/profile/components/Projects";
-import Storage from "views/admin/profile/components/Storage";
-import Upload from "views/admin/profile/components/Upload";
+  // Function to render transaction card
+  const renderTransactionCard = (transaction) => {
+    const cryptoPrices = convertToCrypto(transaction.price);
+    return (
+      <Card key={transaction.id} p="20px">
+        <Flex justify="space-between" mb="20px">
+          <Text fontSize="xl" fontWeight="bold">
+            {transaction.name}
+          </Text>
+          <Text color="gray.500">{transaction.date}</Text>
+        </Flex>
+        <Flex justify="space-between" mb="20px">
+          <Text>{transaction.location}</Text>
+          <Text>${transaction.price.toLocaleString()}</Text>
+        </Flex>
+        <Flex justify="space-between">
+          <Text>BTC {cryptoPrices.btc}</Text>
+          <Text>ETH {cryptoPrices.eth}</Text>
+        </Flex>
+      </Card>
+    );
+  };
 
-// Assets
-import banner from "assets/img/auth/banner.png";
-import avatar from "assets/img/avatars/avatar4.png";
-import React from "react";
-
-export default function Overview() {
   return (
-    <Box pt={{ base: "130px", md: "80px", xl: "80px" }}>
-      {/* Main Fields */}
-      <Grid
-        templateColumns={{
-          base: "1fr",
-          lg: "1.34fr 1fr 1.62fr",
-        }}
-        templateRows={{
-          base: "repeat(3, 1fr)",
-          lg: "1fr",
-        }}
-        gap={{ base: "20px", xl: "20px" }}>
-        <Banner
-          gridArea='1 / 1 / 2 / 2'
-          banner={banner}
-          avatar={avatar}
-          name='Adela Parkson'
-          job='Product Designer'
-          posts='17'
-          followers='9.7k'
-          following='274'
-        />
-        <Storage
-          gridArea={{ base: "2 / 1 / 3 / 2", lg: "1 / 2 / 2 / 3" }}
-          used={25.6}
-          total={50}
-        />
-        <Upload
-          gridArea={{
-            base: "3 / 1 / 4 / 2",
-            lg: "1 / 3 / 2 / 4",
-          }}
-          minH={{ base: "auto", lg: "420px", "2xl": "365px" }}
-          pe='20px'
-          pb={{ base: "100px", lg: "20px" }}
-        />
-      </Grid>
-      <Grid
-        mb='20px'
-        templateColumns={{
-          base: "1fr",
-          lg: "repeat(2, 1fr)",
-          "2xl": "1.34fr 1.62fr 1fr",
-        }}
-        templateRows={{
-          base: "1fr",
-          lg: "repeat(2, 1fr)",
-          "2xl": "1fr",
-        }}
-        gap={{ base: "20px", xl: "20px" }}>
-        <Projects
-          gridArea='1 / 2 / 2 / 2'
-          banner={banner}
-          avatar={avatar}
-          name='Adela Parkson'
-          job='Product Designer'
-          posts='17'
-          followers='9.7k'
-          following='274'
-        />
-        <General
-          gridArea={{ base: "2 / 1 / 3 / 2", lg: "1 / 2 / 2 / 3" }}
-          minH='365px'
-          pe='20px'
-        />
-        <Notifications
-          used={25.6}
-          total={50}
-          gridArea={{
-            base: "3 / 1 / 4 / 2",
-            lg: "2 / 1 / 3 / 3",
-            "2xl": "1 / 3 / 2 / 4",
-          }}
-        />
+    <Box pt={{ base: "120px", md: "80px", xl: "80px" }}>
+      <Grid gap="20px">
+        <Flex
+          flexDirection="column"
+          gridArea={{ xl: "1 / 1 / 2 / 3", "2xl": "1 / 1 / 2 / 2" }}
+        >
+          <Flex
+            mt="45px"
+            mb="20px"
+            justifyContent="space-between"
+            direction={{ base: "column", md: "row" }}
+          >
+            <Text fontSize="2xl" ms="24px" fontWeight="700">
+              Transactions
+            </Text>
+          </Flex>
+          <Flex mb="20px" justify="space-between">
+            <InputGroup maxW="400px">
+              <InputLeftElement
+                pointerEvents="none"
+                children={<SearchIcon color="gray.300" />}
+              />
+              <Input
+                type="text"
+                placeholder="Search transactions..."
+                value={searchQuery}
+                onChange={handleSearchChange}
+              />
+            </InputGroup>
+            <Select
+              maxW="200px"
+              value={filterType}
+              onChange={handleFilterTypeChange}
+            >
+              <option value="all">All Transactions</option>
+              <option value="purchase">Purchases</option>
+              <option value="sale">Sales</option>
+            </Select>
+          </Flex>
+          <SimpleGrid columns={{ base: 1, md: 2 }} gap="20px">
+            {filteredTransactions.map((transaction) =>
+              renderTransactionCard(transaction)
+            )}
+          </SimpleGrid>
+        </Flex>
       </Grid>
     </Box>
   );
-}
+};
+
+export default Transactions;
